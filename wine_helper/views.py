0cf6d1954209d6385c77f5077d10046205a52e00
hitinui <hitinui.robert@enseirb-matmeca.fr>
2016-11-18 15:47:33 +0100

    Hard-coded filters added

--- wine_helper/views.py
+++ wine_helper/views.py
@@ -14,46 +14,20 @@ from messengerbot import MessengerClient, messages, attachments, templates, elem
 
 #  ------------------------ Fill this with your page access token! -------------------------------
 PAGE_ACCESS_TOKEN = "EAAYU6e7AspIBAHvYtRp44RebfWQGlVRUNTTIpqmd27i6nSHCW61noR7yDOrpGlzaRaRO2NreAXful5OlodZAy7xB9Y6SftRW9YfYl4aQ0MPD2HLa3Ey2k6hvfVfEVxuHIMmAkgJ9gnrbdFuVbXr6wMFQzPUteYmk0x5heegZDZD"
-VERIFY_TOKEN = "verify_me"
+VERIFY_TOKEN = "verify_me" # TODO: change token
 
 # Initializing client
 messenger = MessengerClient(access_token=PAGE_ACCESS_TOKEN)
 
-# This function should be outside the BotsView class
 def post_facebook_message(fbid, received_message):
-    # post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + PAGE_ACCESS_TOKEN
-    # response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
-    # status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-    # pprint(status.json())
+    # Warning: "hard-coded" filters. It should be delegate to Wit.ai
     if (received_message == "Bonjour"):
-        recipient = messages.Recipient(recipient_id=fbid)
-        red_button = elements.PostbackButton(
-            title='Rouge',
-            payload='Rouge'
-        )
-        white_button = elements.PostbackButton(
-            title='Blanc',
-            payload='Blanc'
-        )
-        rose_button = elements.PostbackButton(
-            title='Rose',
-            payload='Rose'
-        )
-        template = templates.ButtonTemplate(
-            text='Bonjour, quelle couleur de vin desirez-vous ?',
-            buttons=[
-                red_button, white_button, rose_button
-            ]
-        )
-        attachment = attachments.TemplateAttachment(template=template)
-        message = messages.Message(attachment=attachment)
-        request = messages.MessageRequest(recipient, message)
-        messenger.send(request)
-    else:
-        wine_answer(fbid, received_message)
+        handle_welcome(fbid, received_message)
+    elif received_message == "Rouge" or received_message == "Blanc" or received_message == "Rose":
+        handle_color(fbid, received_message)
 
 
-def wine_answer(fbid, received_message):
+def handle_welcome(fbid, received_message):
     recipient = messages.Recipient(recipient_id=fbid)
     red_button = elements.PostbackButton(
         title='Rouge',
@@ -78,7 +52,16 @@ def wine_answer(fbid, received_message):
     request = messages.MessageRequest(recipient, message)
     messenger.send(request)
 
-# FIXME rename class into something proper...
+
+# TODO: change the function, call API
+def handle_color(fbid, received_message):
+    recipient = messages.Recipient(recipient_id=fbid)
+
+    message = messages.Message(text=received_message)
+    request = messages.MessageRequest(recipient, message)
+    messenger.send(request)
+
+
 class YoMamaBotView(generic.View):
 
     def get(self, request, *args, **kwargs):
@@ -106,11 +89,13 @@ class YoMamaBotView(generic.View):
                     # Print the message to the terminal
                     pprint(message)
                     post_facebook_message(message['sender']['id'], message['message']['text'])
+            # It seems that this condition is never true
             if 'messaging_postbacks' in entry:
                 for message in entry['messaging_postbacks']:
                     if 'postback' in message:
                         post_facebook_message(message['sender']['id'], message['postback']['payload'])
         return HttpResponse()
+
 
 class HelloView(generic.View):
     def get(self, request, *args, **kwargs):
